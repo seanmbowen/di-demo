3781662ba6abc8fc6fa472419e24b5e068cb4421
Sean Bowen <sean.bowen@fullstackitjm.com>
2019-07-15 15:50:38 -0400

    Initial commit

---
+++ src/main/java/com/fsit/didemo/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.fsit.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author sean.bowen
+ * @version 1.0
+ * <p>
+ * Creation date: 2019-07-15
+ * @since 2019
+ */
+@Controller
+public class MyController {
+}
